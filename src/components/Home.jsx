cb091dcd742d7badd9bf913d6638b9b17e13dae1
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-24 00:31:48 +0530

    Home.jsx added (sample) Router DOM Implemented

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+// src/components/Home.jsx
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>Welcome to Popcorn Pal!</h1>
+            <p>Your movie app for everything film-related.</p>
+        </div>
+    );
+};
+
+export default Home;
