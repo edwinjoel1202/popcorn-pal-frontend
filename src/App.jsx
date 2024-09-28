bac2942a5f4943c4c7034abc7a5e51d34e372e5d
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-28 19:52:43 +0530

    App.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
+import First from './components/First';
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<First />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
