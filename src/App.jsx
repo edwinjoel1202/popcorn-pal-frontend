3ec3e9b329fadd7376fa2f411aae924dbc8c33f9
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-10-02 11:11:30 +0530

    UserContext added globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 // src/App.jsx
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import Home from './components/Home';
-import First from './components/First';
-import Movie from './components/Movie';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import First from "./components/First";
+import Movie from "./components/Movie";
+import { UserProvider } from "./context/UserContext";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<First />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/movie/:id" element={<Movie />} />          
-            </Routes>
-        </Router>
-    );
+  return (
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<First />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/movie/:id" element={<Movie />} />
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
 };
 
 export default App;
