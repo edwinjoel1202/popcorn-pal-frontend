5b158c022c2375395dfc9ec822520c123211e37f
edwinjoel1202 <edwinjoel1204@gmail.com>
2025-03-23 20:18:50 +0530

    Navbar as a separated component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ user }) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/home">PopcornPal</Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/home">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/watchlist">Watchlist</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/account">
+                                {user ? `Hi, ${user.username}` : "Account"}
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
